a2f71920030f7cab845cbb43ef1bb5f6293f9b0c
Alexandre DONNART <alexandre.donnart2018@campus-eni.fr>
2019-07-11 11:58:23 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetParking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetParking.Startup))]
+namespace ProjetParking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
